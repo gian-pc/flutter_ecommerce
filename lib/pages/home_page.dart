853e4d26cf556e55b7807a29ac7ee0b814d3f86d
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-11 20:11:17 -0500

    SingleChildScrollView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,28 +28,144 @@ class HomePage extends StatelessWidget {
                   filled: true,
                   suffixIcon: Container(
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16.0),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xffFF7B4D),
-                          Color(0xffF72727),
-                        ],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xffF72727).withOpacity(0.6),
-                          blurRadius: 7,
-                          offset: Offset(0,4),
+                        borderRadius: BorderRadius.circular(16.0),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xffFF7B4D),
+                            Color(0xffF72727),
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
                         ),
-                      ]
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xffF72727).withOpacity(0.6),
+                            blurRadius: 7,
+                            offset: Offset(0, 4),
+                          ),
+                        ]),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
                     ),
-                    child: Icon(Icons.search, color: Colors.white,),
                   ),
                   fillColor: Color(0xffF7F8FA),
                 ),
               ),
+              SizedBox(height: 20),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 140,
+                      margin: EdgeInsets.only(right: 12.0,bottom: 12.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xffFF7B4D),
+                            Color(0xffF72727),
+                          ],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xffF72727).withOpacity(0.6),
+                            blurRadius: 7,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Rapi Bank",
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 220,
+                      margin: EdgeInsets.only(right: 12.0,bottom: 12.0),
+                      padding: EdgeInsets.only(left: 16.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xff131416),
+                            Color(0xff404546),
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Pide tu tarjeta de crédito",
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Te devolvemos hasta el 3% de tus compras",
+                            style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 220,
+                      margin: EdgeInsets.only(right: 12.0,bottom: 12.0),
+                      padding: EdgeInsets.only(left: 16.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xff131416),
+                            Color(0xff404546),
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Pide tu tarjeta de crédito",
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Te devolvemos hasta el 3% de tus compras",
+                            style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
