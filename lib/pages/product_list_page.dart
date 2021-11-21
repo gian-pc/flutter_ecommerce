9a63d1280cf08a20cd8b26f5297f54648e3affbc
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-21 13:20:24 -0500

    List product 4

--- lib/pages/product_list_page.dart
+++ lib/pages/product_list_page.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ProductListPage extends StatefulWidget {
   @override
@@ -7,6 +9,24 @@ class ProductListPage extends StatefulWidget {
 }
 
 class _ProductListPageState extends State<ProductListPage> {
+  List listProduct = [];
+
+  @override
+  initState(){
+    super.initState();
+    getProducts();
+  }
+
+  getProducts() async {
+    String _path = "http://192.168.1.5:8000/api/product/";
+    Uri _uri = Uri.parse(_path);
+    http.Response response = await http.get(_uri);
+    if (response.statusCode == 200) {
+      listProduct = json.decode(response.body);
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,72 +47,72 @@ class _ProductListPageState extends State<ProductListPage> {
         crossAxisCount: 2,
         childAspectRatio: 0.85,
         crossAxisSpacing: 10,
-        children: [
-          Container(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  height: 160,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Colors.transparent,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                          "https://assets.adidas.com/images/w_600,f_auto,q_auto/6a0d6246e26a4852825dad3900baddfd_9366/Zapatillas_adidas_Ultraboost_x_LEGO(r)_Colors_Blanco_FZ3983_01_standard.jpg"),
+        children: listProduct.map<Widget>((e) => Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 160,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.transparent,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://assets.adidas.com/images/w_600,f_auto,q_auto/6a0d6246e26a4852825dad3900baddfd_9366/Zapatillas_adidas_Ultraboost_x_LEGO(r)_Colors_Blanco_FZ3983_01_standard.jpg"),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      right: 10.0,
+                      top: 10.0,
+                      child: Icon(
+                        Icons.more_vert,
+                      ),
                     ),
-                  ),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        right: 10.0,
-                        top: 10.0,
-                        child: Icon(
-                          Icons.more_vert,
-                        ),
-                      ),
-                    ],
-                  ),
+                  ],
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Zapatillas Supercourt",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xff121212),
-                              ),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Zapatillas Supercourt",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff121212),
                             ),
-                            SizedBox(height: 6,),
-                            Text(
-                              "S/. 299.00",
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 13.0,
-                                color: Color(0xff121212),
-                              ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            "S/. 299.00",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                              color: Color(0xff121212),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      Icon(Icons.favorite_border)
-                    ],
-                  ),
+                    ),
+                    Icon(Icons.favorite_border)
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),).toList(),
       ),
     );
   }
