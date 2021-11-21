e86494f6495bfb8046285ed4662f8e56ad237abe
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-21 12:37:03 -0500

    List product 2

--- lib/pages/product_list_page.dart
+++ lib/pages/product_list_page.dart
@@ -24,6 +24,7 @@ class _ProductListPageState extends State<ProductListPage> {
       ),
       body: GridView.count(
         crossAxisCount: 2,
+        childAspectRatio: 0.85,
         children: [
           Container(
             child: Column(
@@ -54,33 +55,209 @@ class _ProductListPageState extends State<ProductListPage> {
                 ),
                 Row(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Zapatillas Supercourt scwdcwcw",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff121212),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Zapatillas Supercourt scwdcwcw",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff121212),
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
                           ),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Text(
-                          "S/. 299.00",
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 13.0,
-                            color: Color(0xff121212),
+                          Text(
+                            "S/. 299.00",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                              color: Color(0xff121212),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     )
                   ],
                 ),
               ],
             ),
-          )
+          ),
+          Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.redAccent,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://assets.adidas.com/images/w_600,f_auto,q_auto/6a0d6246e26a4852825dad3900baddfd_9366/Zapatillas_adidas_Ultraboost_x_LEGO(r)_Colors_Blanco_FZ3983_01_standard.jpg"),
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        right: 10.0,
+                        top: 10.0,
+                        child: Icon(
+                          Icons.more_vert,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Zapatillas Supercourt scwdcwcw",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff121212),
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            "S/. 299.00",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                              color: Color(0xff121212),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.redAccent,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://assets.adidas.com/images/w_600,f_auto,q_auto/6a0d6246e26a4852825dad3900baddfd_9366/Zapatillas_adidas_Ultraboost_x_LEGO(r)_Colors_Blanco_FZ3983_01_standard.jpg"),
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        right: 10.0,
+                        top: 10.0,
+                        child: Icon(
+                          Icons.more_vert,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Zapatillas Supercourt scwdcwcw",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff121212),
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            "S/. 299.00",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                              color: Color(0xff121212),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.redAccent,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://assets.adidas.com/images/w_600,f_auto,q_auto/6a0d6246e26a4852825dad3900baddfd_9366/Zapatillas_adidas_Ultraboost_x_LEGO(r)_Colors_Blanco_FZ3983_01_standard.jpg"),
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        right: 10.0,
+                        top: 10.0,
+                        child: Icon(
+                          Icons.more_vert,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Zapatillas Supercourt scwdcwcw",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff121212),
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            "S/. 299.00",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13.0,
+                              color: Color(0xff121212),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
