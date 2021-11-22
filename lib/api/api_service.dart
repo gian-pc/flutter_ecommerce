2d3288762b23d68c98fb217dd5de04dff8545595
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-22 12:34:11 -0500

    Create Banner model 2

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,19 +1,21 @@
 
 import 'dart:convert';
 
+import 'package:flutter_codigo3_ecommerce/models/banner_model.dart';
 import 'package:flutter_codigo3_ecommerce/utils/constants.dart';
 import 'package:http/http.dart' as http;
 
 
 class APIService {
 
- Future<List> getBanners() async {
-    List listBanner = [];
+ Future<List<Banner>> getBanners() async {
+    List<Banner> listBanner = [];
     String _path = "$pathApi/banner/";
     Uri _uri = Uri.parse(_path);
     http.Response response = await http.get(_uri);
     if (response.statusCode == 200) {
-      listBanner = json.decode(response.body);
+      listBanner = json.decode(response.body).map<Banner>((item)=>Banner.fromJson(item)).toList();
+      print(listBanner);
       return listBanner;
     }
     return listBanner;
