519cfff42cad7a2522be75cd7a41b7e6ae47ac3b
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-21 19:28:30 -0500

    Detail Page 7

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -25,151 +25,207 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12.withOpacity(0.1),
-                    offset: Offset(0, 4),
-                    blurRadius: 10.0,
-                  ),
-                ],
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40)),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/cd6bed3b44e340938495ac7d0106ca60_9366/Tenis_Ultraboost_21_Amarillo_FY0373_01_standard.jpg"),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 16),
-                  child: Text(
-                    "ADIDAS",
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 14.0,
+                Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12.withOpacity(0.1),
+                        offset: Offset(0, 4),
+                        blurRadius: 10.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40)),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/cd6bed3b44e340938495ac7d0106ca60_9366/Tenis_Ultraboost_21_Amarillo_FY0373_01_standard.jpg"),
                     ),
                   ),
                 ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 16),
-                  child: Text(
-                    "ZAPATILLAS SUPERCOURI",
-                    style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: 16.0,
-                        fontFamily: 'Comfortaa-Bold'),
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-                Icon(
-                  CupertinoIcons.checkmark_seal_fill,
-                  color: Colors.black54,
-                  size: 20.0,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                    "S/ 299.00",
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        onTap: _quantity == 0
-                            ? () {}
-                            : () {
-                                _quantity--;
-                                setState(() {});
-                              },
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            color: _quantity == 0
-                                ? Colors.black38
-                                : Color(0xff121212),
-                            borderRadius: BorderRadius.circular(14.0),
-                          ),
-                          child: Icon(
-                            CupertinoIcons.minus,
-                            color: Colors.white,
-                          ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text(
+                        "ADIDAS",
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 14.0,
                         ),
                       ),
-                      Container(
-                        width: 40,
-                        height: 40,
-                        alignment: Alignment.center,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text(
+                        "ZAPATILLAS SUPERCOURI",
+                        style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 16.0,
+                            fontFamily: 'Comfortaa-Bold'),
+                      ),
+                    ),
+                    Icon(
+                      CupertinoIcons.checkmark_seal_fill,
+                      color: Colors.black54,
+                      size: 20.0,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        "S/ 299.00",
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: _quantity == 0
+                                ? () {}
+                                : () {
+                              _quantity--;
+                              setState(() {});
+                            },
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: _quantity == 0
+                                    ? Colors.black38
+                                    : Color(0xff121212),
+                                borderRadius: BorderRadius.circular(14.0),
+                              ),
+                              child: Icon(
+                                CupertinoIcons.minus,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 40,
+                            height: 40,
+                            alignment: Alignment.center,
+                            child: Text(
+                              _quantity.toString(),
+                              style: TextStyle(
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              _quantity++;
+                              setState(() {});
+                            },
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: Color(0xff121212),
+                                borderRadius: BorderRadius.circular(14.0),
+                              ),
+                              child: Icon(
+                                CupertinoIcons.plus,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+
+                SizedBox(
+                  height: 16.0,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Text(
+                        "Descripción general",
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(
+                  height: 16.0,
+                ),
+
+                Row(
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
                         child: Text(
-                          _quantity.toString(),
+                          "Tomemos un momento para rendir homenaje a un ícono. ¿Será la leyenda que desafiaba la gravedad en los años 80? ¿O tal vez las zapatillas que le daban estatus a los pies de raperos? De hecho, ambas. Las zapatillas adidas Forum han dominado las canchas de básquet y las calles, y ahora regresan con una versión de corte medio para llevar tus pasos a otro nivel. Envuelve tus pies con un estilo inconfundible en piel revestida prémium para un look que exude clase.",
                           style: TextStyle(
-                            fontSize: 20.0,
+                            color: Colors.black54,
+                            fontSize: 14.0,
                           ),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          _quantity++;
-                          setState(() {});
-                        },
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            color: Color(0xff121212),
-                            borderRadius: BorderRadius.circular(14.0),
-                          ),
-                          child: Icon(
-                            CupertinoIcons.plus,
-                            color: Colors.white,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
+                        child: Text(
+                          "Tomemos un momento para rendir homenaje a un ícono. ¿Será la leyenda que desafiaba la gravedad en los años 80? ¿O tal vez las zapatillas que le daban estatus a los pies de raperos? De hecho, ambas. Las zapatillas adidas Forum han dominado las canchas de básquet y las calles, y ahora regresan con una versión de corte medio para llevar tus pasos a otro nivel. Envuelve tus pies con un estilo inconfundible en piel revestida prémium para un look que exude clase.",
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 14.0,
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                )
+                    ),
+                  ],
+                ),
+
+                SizedBox(
+                  height: 40.0,
+                ),
               ],
             ),
-            // Row(
-            //   children: [
-            //     Padding(
-            //       padding: const EdgeInsets.only(left: 16),
-            //       child: Text(
-            //         "Tomemos un momento para rendir homenaje a un ícono. ¿Será la leyenda que desafiaba la gravedad en los años 80? ¿O tal vez las zapatillas que le daban estatus a los pies de raperos? De hecho, ambas. Las zapatillas adidas Forum han dominado las canchas de básquet y las calles, y ahora regresan con una versión de corte medio para llevar tus pasos a otro nivel. Envuelve tus pies con un estilo inconfundible en piel revestida prémium para un look que exude clase.",
-            //         style: TextStyle(
-            //           color: Colors.black54,
-            //           fontSize: 18.0,
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+              onPressed: (){},
+              child: Text("Hola"),
+            ),
+          )
+        ],
       ),
     );
   }
