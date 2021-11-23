945af82b77d789926dd722e33cb5c6a0298818f9
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-22 23:25:28 -0500

    db Database 2

--- lib/db/db_manager.dart
+++ lib/db/db_manager.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter_codigo3_ecommerce/models/product_model.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -28,6 +29,19 @@ class DBManager{
         await db.execute("CREATE TABLE Product(id INTEGER, brand TEXT, name TEXT, price REAL, quantity INTEGER, image TEXT)");
       }
     );
+  }
+  
+  Future<int> insertProduct(ProductModel productModel)async{
+   final db = await database;
+   int res = await db!.insert("Product", productModel.toJson());
+   return res;
+  }
+
+  Future<List<ProductModel>> getAllProducts()async{
+   final db = await database;
+   List<Map<String,dynamic>> res = await db!.query("Product");
+   List<ProductModel> listProductModel = res.isNotEmpty? res.map((e) => ProductModel.fromJson(e)).toList():[];
+   return listProductModel;
 
   }
 
